520cd4bbf207a34126ba2720802f153f81ad91f6
Mohit Manuja <manuja.mohit@gmail.com>
2020-03-25 16:00:14 +0530

    Client View Almost Ready

---
+++ app/src/main/java/com/epass/curfue/activities/VerificationActivity.kt
@@ -0,0 +1,22 @@
+package com.epass.curfue.activities
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.epass.curfue.R
+import com.epass.curfue.databinding.ActivityApplicationCreatedBinding
+import com.epass.curfue.databinding.ActivityCreateApplicationBinding
+
+class VerificationActivity :BaseActivity(){
+
+    lateinit var binding:ActivityApplicationCreatedBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_application_created)
+        supportActionBar?.title = "Anumati Granted"
+
+
+    }
+
+
+}
