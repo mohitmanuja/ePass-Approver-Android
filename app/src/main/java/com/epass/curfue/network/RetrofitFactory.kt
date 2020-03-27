47549c24a3786b8e7aab37d3e23f0bfae0c0a103
Mohit Manuja <manuja.mohit@gmail.com>
2020-03-27 11:42:25 +0530

    Pointing changed to https://epass.egovernments.org:8091

--- app/src/main/java/com/epass/curfue/network/RetrofitFactory.kt
+++ app/src/main/java/com/epass/curfue/network/RetrofitFactory.kt
@@ -8,14 +8,14 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitFactory {
-    const val BASE_URL = "http://13.233.33.10:8091"
+    const val BASE_URL = "https://epass.egovernments.org:8091"
     var client: OkHttpClient? = null
 
 
 
     fun makeRetrofitService(context: Context): RetrofitService {
         if (client == null){
-            client = NetworkUtil.getUnsafeOkHttpClient().connectTimeout(20, TimeUnit.SECONDS)
+            client =  OkHttpClient.Builder().connectTimeout(20, TimeUnit.SECONDS)
                 .readTimeout(30, TimeUnit.SECONDS)
                 .writeTimeout(30, TimeUnit.SECONDS)
 //                .addInterceptor(ChuckInterceptor(context))
